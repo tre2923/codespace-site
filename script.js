a66d17cfd7cd1e2cae30ea31180d84cb83914656
tre2923 <tre2923@proton.me>
2024-11-23 00:31:15 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the button element
+const changeColorBtn = document.getElementById('changeColorBtn');
+
+// Add event listener to the button
+changeColorBtn.addEventListener('click', function() {
+    // Change the background color of the body
+    document.body.style.backgroundColor = '#a1c4fd';
+});
